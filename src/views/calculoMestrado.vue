a193c8f6570ba5cebecb9fcafbf4eec87ed5db38
alana <alana.araujo@tce.ma.gov.br>
2022-03-31 14:28:30 -0300

    .

--- src/views/calculoMestrado.vue
+++ src/views/calculoMestrado.vue
@@ -1,59 +1,155 @@
 <template>
-        <v-list three-line>
-          <h2 style="justify-content: center; display: flex; align-items: center; margin-bottom: 20px">Calculo de Indices do Mestrado</h2>
-          <div>
-            <v-row style="justify-content: center; display: flex; align-items: center">
-              <v-col cols="12" md="2">
-                <v-select
-                  :items="years"
-                  label="Ano Inicio"
-                  v-model="anoInicio"
-                  @change="mostrarTabela = false"
-                ></v-select>
-              </v-col>
-              <v-col cols="12" md="2">
-                <v-select
-                  :items="years"
-                  label="Ano Final"
-                  v-model="anoFinal"
-                  @change="mostrarTabela = false"
-                ></v-select>
-              </v-col>
-              <v-col cols="12" md="1">
-                <v-btn color="primary"
-                  @click="calcularIndices()"
-                  :disabled="habilitar"
-                > Calcular</v-btn>
-              </v-col>
-            </v-row>
-          </div>
-          <div v-if="mostrarTabela==true">
-            <v-data-table
-                
+  <v-app>
+    <v-list three-line>
+      <h2 style="justify-content: center; display: flex; align-items: center; margin-bottom: 20px">Calculo de Indices do Mestrado</h2>
+      <div>
+        <v-row style="justify-content: center; display: flex; align-items: center">
+          <v-col cols="12" md="2">
+            <v-select
+              :items="years"
+              label="Ano Inicio"
+              v-model="anoInicio"
+              @change="mostrarTabela = false"
+            ></v-select>
+          </v-col>
+          <v-col cols="12" md="2">
+            <v-select
+              :items="years"
+              label="Ano Final"
+              v-model="anoFinal"
+              @change="mostrarTabela = false"
+            ></v-select>
+          </v-col>
+          <v-col cols="12" md="1">
+            <v-btn color="primary"
+              @click="calcularIndices()"
+              :disabled="habilitar"
+            > Calcular</v-btn>
+          </v-col>
+        </v-row>
+      </div>
+      <div v-if="mostrarTabela==true" style="margin: 20px">
+        <v-tabs
+          v-model="model"
+          centered
+          slider-color="yellow"
+        >
+
+          <v-tab :key="1" :href="`#tab-1`">Tabelas</v-tab>
+          <v-tab :key="2" :href="`#tab-2`">Gráficos</v-tab>
+
+        </v-tabs>
+        <v-tabs-items v-model="model">
+          <v-tab-item :key="1" :value="`tab-1`">
+            <v-card>
+              <h3 style="justify-content: center; display: flex; align-items: center; margin-top: 20px">Calculo de Indices dos Professores</h3>
+              <v-data-table
                 :headers="headers"
                 :items="items"
-                :items-per-page="5"
-            ></v-data-table>
-            <h2 style="justify-content: center; display: flex; align-items: center; margin-top: 20px">Calculo Geral dos Anos ({{anoInicio}} - {{anoFinal}})</h2>
-            <v-data-table
+                :page.sync="page"
+                :items-per-page="itemsPerPage"
+                hide-default-footer
+                @page-count="pageCount = $event"
+                  
+              ></v-data-table>
+              <div class="text-center pt-2">
+                <v-pagination
+                  v-model="page"
+                  :length="pageCount"
+                ></v-pagination>
+              </div>
+            </v-card>
+            <v-card>
+              <h3 style="color: dark;justify-content: center; display: flex; align-items: center; margin-top: 20px">Calculo de Indices do PPGCC dos Anos ({{anoInicio}} - {{anoFinal}})</h3>
+              <v-data-table
                 :headers="titulo"
                 :items="ppgcc"
                 :items-per-page="5"
-            ></v-data-table>
-          </div>
-        </v-list>
+                hide-default-footer
+                class="elevation-3"
+              ></v-data-table>
+            </v-card>
+          </v-tab-item>
+          <v-tab-item :key="2" :value="`tab-2`">
+              <div style="display: block">
+                <mdb-bar-chart :data="barChartData" :options="barChartOptions" :height="500"/>
+              </div>
+          </v-tab-item>
+        </v-tabs-items>
+      </div>
+
+        
+    </v-list>
+  </v-app>
 </template>
 
 <script>
-
+// import * as MestradoService from '../services/MestradoService'
 import * as CalculoService from '../services/CalculoService'
+import {mdbBarChart} from 'mdbvue'
 
   export default {
     props:{
         value: Object
     },
+    components:{
+      mdbBarChart,
+    },
     data () {
       return {
+        barChartData: {
+        labels: [],
+        datasets: [
+          {
+            label: 'iRegistro',
+            data: [20],
+            backgroundColor: 'rgba(245, 74, 85, 0.5)',
+            borderWidth: 1
+          }, {
+            label: 'iNao Resgistro',
+            data: [],
+            backgroundColor: 'rgba(90, 173, 246, 0.5)',
+            borderWidth: 1
+          }, {
+            label: 'iGeral',
+            data: [],
+            backgroundColor: 'rgba(245, 192, 50, 0.5)',
+            borderWidth: 1
+          }
+        ]
+      },
+      barChartOptions: {
+        responsive: true,
+        maintainAspectRatio: false,
+        scales: {
+          xAxes: [{
+            barPercentage: 1,
+            gridLines: {
+              display: true,
+              color: 'rgba(0, 0, 0, 0.1)'
+            }
+          }],
+          yAxes: [{
+            gridLines: {
+              display: true,
+              color: 'rgba(0, 0, 0, 0.1)'
+            },
+            ticks: {
+              beginAtZero: true
+            }
+          }]
+        }
+      },
+
+
+
+
+
+
+        page: 1,
+        pageCount: 0,
+        itemsPerPage: 5,
+        model: 'tab-1',
         disabled: true,
         headers: [
           {
@@ -102,6 +198,11 @@ import * as CalculoService from '../services/CalculoService'
       calcularIndices(){
         CalculoService.calculoIndices(this.anoInicio,this.anoFinal).then((res)=>{
           this.items = res.data
+          this.items.forEach(element =>{
+            this.barChartData.labels.push(element.nomeCompleto)
+            this.barChartData.datasets[0].data.push(element.iRestrito)
+            console.log(this.barChartData)
+          })
           this.mostrarTabela = true
         })
         CalculoService.calculoIndicesPPGCC(this.anoInicio,this.anoFinal).then((res)=>{
